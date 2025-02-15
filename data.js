729dedc7ed3c3373cbc545ec2e7b79abd549c204
mmjafet <mmjafet01dic02@gmail.com>
2025-02-15 12:34:53 -0600

    agregando data.js

---
+++ data.js
@@ -0,0 +1,15 @@
+module.exports.users = [
+
+    {
+        id: 1,
+        username: 'jafet',
+        pasword: '1234',
+    },
+    {
+        id: 1,
+        username: 'suseth',
+        pasword: '1234',
+    },
+];    
+    
+    
