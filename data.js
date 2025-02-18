ddd9fa4d916aee880940d2ff3a03c7e50322d461
mmjafet <mmjafet01dic02@gmail.com>
2025-02-17 22:18:50 -0600

    agregando datos de usuarios al archivo data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+module.exports.users = [
+    {
+        id:1,
+        username:"Lucio",
+        password:"123"
+    },
+    {
+        id:2,
+        username:"Lucio2",
+        password:"1234"
+    },
+];
